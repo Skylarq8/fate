6c8adc4c8231d894132d5fc6fea159808afd5bfd
Nrn Badar-Uugan <macbook@Skylarqs-MacBook-Pro.local>
2026-03-04 08:35:59 +0800

    main page little changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { HeroHeader } from "@/components/header";
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 import ProductCard from "@/components/ProductCard";
 import { products } from "@/lib/product";
-import { div } from "motion/react-client";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex mb-2 justify-between items-baseline-last">
-        <h1 className="text-2xl font-heading">Best Seller</h1>
-        <h1 className="font-body text-gray-300 text-sm">see all</h1>
+      <div>
+        <div className="flex mb-3 sm:mb-4 justify-between items-baseline-last">
+          <h1 className="text-[20px] sm:text-2xl font-heading font-bold">Онцлох бүтээгдэхүүн</h1>
+          <h1 className="font-body text-gray-300 text-[12px] sm:text-[13px] lg:text-[14px] border-b border-current inline-block">Бүгдийг харах</h1>
+        </div>
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4 lg:gap-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} title={product.title} image={product.image} price={product.price} id={0}/>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4 lg:gap-4">
-        {products.map((product) => (
-          <ProductCard key={product.id} title={product.title} image={product.image} price={product.price} id={0}/>
-        ))}
+      <div>
+        <div className="flex mt-7 mb-3 sm:mb-4 justify-between items-baseline-last">
+          <h1 className="text-[20px] sm:text-2xl font-heading font-bold">Шинэ бүтээгдэхүүн</h1>
+          <h1 className="font-body text-gray-300 text-[12px] sm:text-[13px] lg:text-[14px] border-b border-current inline-block">Бүгдийг харах</h1>
+        </div>
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4 lg:gap-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} title={product.title} image={product.image} price={product.price} id={0}/>
+          ))}
+        </div>
       </div>
     </div>
   );
